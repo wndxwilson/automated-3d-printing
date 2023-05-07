52a425cf161b9efd56520335fa2c44a29b6952bb
wilson <taiwilson5@gmail.com>
2023-05-07 12:20:13 +0800

    init commit

---
+++ local-server/local-server/local_server.py
@@ -0,0 +1,27 @@
+import socketio
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+sio = socketio.Client()
+
+
+@sio.event
+def connect():
+    print("connection established")
+
+
+@sio.event
+def my_message(data):
+    print("message received with ", data)
+    sio.emit("my response", {"response": "my response"})
+
+
+@sio.event
+def disconnect():
+    print("disconnected from server")
+
+
+sio.connect("http://localhost:5000")
+sio.wait()
